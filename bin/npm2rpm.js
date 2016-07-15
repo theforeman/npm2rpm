354a60abda02a0e876d164e65a41011ea9617d2b
Daniel Lobato <elobatocs@gmail.com>
2016-07-15 18:17:38 +0200

    Stream tgz to extract

--- bin/npm2rpm.js
+++ bin/npm2rpm.js
@@ -12,12 +12,16 @@ npm2rpm
   .option('-t, --template [template]', "RPM .spec template to use", 'default.n2r')
   .parse(process.argv);
 
-npmModule(npm2rpm.name, npm2rpm.version);
-//var spec_file = fs.readFileSync(npm2rpm.template, { encoding: 'utf8' });
-//spec_file = replaceAttribute(spec_file, '\\$NAME', npm2rpm.name);
-//spec_file = replaceAttribute(spec_file, '\\$VERSION', npm2rpm.version);
-//spec_file = replaceAttribute(spec_file, '\\$RELEASE', npm2rpm.release);
-//console.log(spec_file);
+var npm_module = new npmModule(npm2rpm.name, npm2rpm.version);
+var spec_file = fs.readFileSync(npm2rpm.template, { encoding: 'utf8' });
+spec_file = replaceAttribute(spec_file, '\\$NAME', npm2rpm.name);
+spec_file = replaceAttribute(spec_file, '\\$VERSION', npm2rpm.version);
+spec_file = replaceAttribute(spec_file, '\\$RELEASE', npm2rpm.release);
+spec_file = replaceAttribute(spec_file, '\\$LICENSE', npm_module.license);
+spec_file = replaceAttribute(spec_file, '\\$SUMMARY', npm_module.summary);
+spec_file = replaceAttribute(spec_file, '\\$PROJECTURL', npm_module.project_url);
+spec_file = replaceAttribute(spec_file, '\\$DESCRIPTION', npm_module.description);
+console.log(spec_file);
 
 function replaceAttribute(data, template_attr, replacement_text) {
   return data.replace(new RegExp(template_attr, 'g'), replacement_text);
@@ -26,29 +30,48 @@ function replaceAttribute(data, template_attr, replacement_text) {
 function npmModule(name, version) {
   this.name = name;
   this.version = version;
-  this.url = 'http://registry.npmjs.org/' + name + '/-/' +
-               name + '-' + version + '.tgz';
-  downloadFromNPM();
-  this.summary = '';
-  this.license = '';
-  this.project_url = '';
-  this.description = '';
-  this.files_to_copy = '';
-  this.docfiles = '';
+  this.url = 'http://registry.npmjs.org/' + name + '/-/' + name + '-' + version + '.tgz';
+
+  var tar_stream = extractTar(downloadFromNPM())
+	tar_stream.on('error', (error) => {
+		console.log(error);
+	})
+  tar_stream.on('finish', () => {
+		console.log('Finished extracting module');
+    var package_json = JSON.parse(fs.readFileSync(this.tmp_dir + '/package/package.json'));
+    set('description', package_json['description']);
+    set('license', package_json['license']);
+//		this.description = package_json['description'];
+//		this.summary = this.description.split('\n')[0];
+//		this.license = package_json['license'];
+//		this.project_url = package_json['homepage']; ;
+//		this.dependencies = package_json['dependencies'];
+	})
+
+	this.files_to_copy = '';
+	this.docfiles = '';
 
   function downloadFromNPM() {
     var request = require('request');
-    var tar = require('tar.gz');
-    var tmp = require('tmp');
-		var tmpDir = tmp.dirSync({ mode: 0644, prefix: 'npm2rpm-', keep: true });
-    tmpDir.removeCallback();
-		console.log('Starting npm module ' + url + ' download to ' + tmpDir.name);
-    console.log('Unpacking the tar...');
-    var npm_file = request.get(this.url)
-    var write_dir = tar().createWriteStream(tmpDir.name)
-    npm_file.pipe(write_dir);
+	  console.log('Starting npm module download: ' + url);
+		return request.get(this.url);
+  }
 
-    //fs.createReadStream(tmpFile.name).pipe(tar.extract(tmpFile.name.slice(0, -4)))
-    console.log('Tar unpacked!');
+  function createTempDir() {
+    var tmp = require('tmp');
+		var tmpDir = tmp.dirSync(
+			{ mode: 6644,
+				prefix: 'npm2rpm-',
+				keep: true });
+    this.tmp_dir = tmpDir.name;
+    return tmpDir.name;
+  }
+
+  function extractTar(request_stream) {
+    var targz = require('tar.gz');
+		var tmpDir = createTempDir();
+		var write_stream = targz().createWriteStream(tmpDir);
+    console.log('Unpacking in ' + tmpDir + ' ...');
+		return request_stream.pipe(write_stream)
   }
 }
