5302a7fc9ed685eeb43f620cd8fb474450c10414
Daniel Lobato <elobatocs@gmail.com>
2016-07-19 11:57:41 +0200

    Download files asynchronously

--- bin/npm2rpm.js
+++ bin/npm2rpm.js
@@ -5,6 +5,7 @@ var npm2rpm = require('commander');
 var helpers = require('../lib/npm_helpers.js');
 var npmModule = require('../lib/npm_module.js');
 var specFileGenerator = require('../lib/spec_file_generator.js');
+var async = require('async');
 
 console.log('---- npm2rpm ----'.green.bold);
 console.log('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-'.rainbow.bgWhite);
@@ -19,7 +20,7 @@ var tar_extract = helpers.extractTar(helpers.downloadFromNPM(npm2rpm.name, npm2r
 tar_extract['stream'].on('error', (error) => {
 	console.log(error);
 })
-tar_extract['stream'].on('finish', (results) => {
+tar_extract['stream'].on('finish', () => {
 	console.log(' - Finished extracting for '.bold + npm2rpm.name);
 	console.log(' - Reading package.json for '.bold + npm2rpm.name);
 	var package_json = JSON.parse(fs.readFileSync(tar_extract['location'] + '/package/package.json'));
@@ -47,8 +48,26 @@ tar_extract['stream'].on('finish', (results) => {
 			fs.mkdirSync('npm2rpm/SPECS');
     fs.writeFile('npm2rpm/SPECS/nodejs-' + npm_module.name + '.spec', spec_file);
 
-    var async = require('async');
-    console.log(spec_file);
+    async.each(dependencies, (file, callback) => {
+      var download_location = fs.createWriteStream('npm2rpm/SOURCES/' + file[0] + '-' + file[1] + '.tgz');
+      var download_pipe = helpers.downloadFromNPM(file[0], file[1]).pipe(download_location);
+      download_pipe.on('finish', () => {
+        console.log('   - ' + file[0] + '-' + file[1] + ' finished'.green);
+        callback(null)
+      });
+      download_pipe.on('error', (error) => {
+        console.log('   - ' + file[0] + '-' + file[1] + ' failed to download'.red);
+        callback(error);
+      })
+    }, (err) => {
+      if(err) {
+        console.log('Error: '.bold.red + err + ' failed to download'.bold.red);
+      } else {
+        console.log('All files have been processed successfully'.white.underline);
+        console.log('Check out npm2rpm/SOURCES and npm2rpm/SPECS for the results.');
+      }
+    });
+    //console.log(spec_file);
   });
 })
 
